74a6b971335d7136b067d5a364d6dc56e6e6273d
SH Ahn <shahn@SHui-iMac.local>
2020-12-28 23:18:36 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://iwishash:vpdlsxj@cluster0.jqfpm.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB is connected successfully."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
