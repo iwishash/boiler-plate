b59f65b9dedc0cca918268f03fe7e08e221646f6
SH Ahn <shahn@SHui-iMac.local>
2020-12-29 02:47:08 +0900

    second commit

--- index.js
+++ index.js
@@ -2,23 +2,41 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
 const mongoose = require("mongoose");
+const config = require("./config/key");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://iwishash:vpdlsxj@cluster0.jqfpm.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB is connected successfully."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) {
+      return res.json({ success: false, err });
+    } else {
+      return res.status(200).json({ success: true });
+    }
+  });
 });
 
 app.listen(port, () => {
